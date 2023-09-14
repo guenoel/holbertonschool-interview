2881f62459bc56d44beedefdf6c9c671bdfcb652
guenoel <guenooel@hotmail.com>
2023-09-14 12:31:21 -0700

    first try

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,6 +1,6 @@
 #include "sandpiles.h"
 
-void print_grid(int grid[3][3])
+void print_grid2(int **grid)
 {
 	int i, j;
 
@@ -16,31 +16,50 @@ void print_grid(int grid[3][3])
 	}
 }
 
-void sandpile_reduc(int grid[3][3])
+int **sandpile_reduc(int **grid)
 {
-	int i, j;
+	int i, j, num;
+	int **next_grid;
+	next_grid = (int **)malloc(sizeof(int *) * 3);
+	for (int i = 0; i < 3; i++)
+		next_grid[i] = (int *)malloc(sizeof(int) * 3);
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (grid[i][j] > 3)
+			num = grid[i][j];
+			if (num <= 3)
 			{
-				grid[i][j] -= 4;
-				if (i - 1 >= 0)
-					grid[i - 1][j] += 1;
-				if (j - 1 >= 0)
-					grid[i][j - 1] += 1;
-				if (i + 1 < 3)
-					grid[i + 1][j] += 1;
-				if (j + 1 < 3)
-					grid[i][j + 1] += 1;
+				next_grid[i][j] = grid[i][j];
 			}
 		}
 	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			num = grid[i][j];
+			if (num > 3)
+			{
+				next_grid[i][j] += (num - 4);
+				if (i - 1 >= 0)
+					next_grid[i - 1][j]++;
+				if (j - 1 >= 0)
+					next_grid[i][j - 1]++;
+				if (i + 1 < 3)
+					next_grid[i + 1][j]++;
+				if (j + 1 < 3)
+					next_grid[i][j + 1]++;
+			}
+		}
+	}
+	free(grid);
+	return (next_grid);
 }
 
-int sandpiles_check(int grid[3][3])
+int sandpiles_check(int **grid)
 {
 	int i, j;
 
@@ -57,7 +76,11 @@ int sandpiles_check(int grid[3][3])
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
-	int grid_sum[3][3] = {0};
+	int **grid_sum;
+	grid_sum = (int **)malloc(sizeof(int *) * 3);
+	for (int i = 0; i < 3; i++)
+		grid_sum[i] = (int *)malloc(sizeof(int) * 3);
+
 	printf("=\n");
 	for (i = 0; i < 3; i++)
 	{
@@ -71,7 +94,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	while(sandpiles_check(grid_sum))
 	{
 		printf("=\n");
-		sandpile_reduc(grid_sum);
-		print_grid(grid_sum);
+		grid_sum = sandpile_reduc(grid_sum);
+		print_grid2(grid_sum);
 	}
+	free(grid_sum);
 }
