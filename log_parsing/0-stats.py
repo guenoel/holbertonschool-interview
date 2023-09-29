a5d05c14db0962f25265c41d71ad429f351807c6
guenoel <guenooel@hotmail.com>
2023-09-29 08:59:43 -0700

    try except

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -6,6 +6,7 @@ Exercice Log parsing
 """
 
 import sys
+import traceback
 
 
 def main():
@@ -15,20 +16,33 @@ def main():
     size = 0
     counter = 0
     status_map = {}
-    for line in sys.stdin:
-        """ jose """
-        line_parsed = line.split(" ")
-        if len(line_parsed) >= 8:
-            key = line_parsed[7]
-            status_map[key] = status_map.get(key, 0) + 1
-        counter += 1
-        if len(line_parsed) >= 9:
-            size += int(line_parsed[8])
-        if counter % 10 == 0:
-            print("File size: {}".format(size))
-            for key, value in sorted(status_map.items()):
-                print("{}: {}".format(key, value))
+    try:
+        for line in sys.stdin:
+            """ jose """
+            line_parsed = line.split(" ")
+            if len(line_parsed) >= 8:
+                key = line_parsed[7]
+                status_map[key] = status_map.get(key, 0) + 1
+            counter += 1
+            if len(line_parsed) >= 9:
+                size += int(line_parsed[8])
+            if counter % 10 == 0:
+                print_result(size, status_map)
+    except KeyboardInterrupt:
+        """ Keyboard interruption """
+        traceback.print_exc()  # Print the traceback when interrupted
+        sys.exit(1)  # Exit with a non-zero status code
 
+    finally:
+        """ """
+        # Print the final statistics
+        print_result(size, status_map)
+
+def print_result(size, status_map):
+    """ print_result """
+    print("File size: {}".format(size))
+    for key, value in sorted(status_map.items()):
+        print("{}: {}".format(key, value))
 
 if __name__ == "__main__":
     main()
