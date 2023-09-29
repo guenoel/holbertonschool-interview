bd0621a66eb09095b69c12921732e2b2af3462ba
guenoel <guenooel@hotmail.com>
2023-09-29 06:11:14 -0700

    comment

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -7,16 +7,19 @@ import sys
 size = 0
 counter = 0
 status_map = {}
-for line in sys.stdin:
-    line_parsed = line.split(" ")
-    if len(line_parsed) >= 8:
-        key = line_parsed[7]
-        status_map[key] = status_map.get(key, 0) + 1
-    counter += 1
-    if len(line_parsed) >= 9:
-        size += int(line_parsed[8])
-    if counter > 10:
-        print(f"File size: {size}")
-        for key, value in status_map.items():
-            print(f"{key}: {value}")
-        counter = 0
+
+def main():
+    """ comment main """
+    for line in sys.stdin:
+        line_parsed = line.split(" ")
+        if len(line_parsed) >= 8:
+            key = line_parsed[7]
+            status_map[key] = status_map.get(key, 0) + 1
+        counter += 1
+        if len(line_parsed) >= 9:
+            size += int(line_parsed[8])
+        if counter > 10:
+            print(f"File size: {size}")
+            for key, value in status_map.items():
+                print(f"{key}: {value}")
+            counter = 0
