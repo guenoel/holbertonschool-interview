d8ac9f141498359820a5cbaa243e583030b44a09
guenoel <guenooel@hotmail.com>
2023-09-29 11:53:46 -0700

    add str replacement

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -18,17 +18,21 @@ def main():
     counter = 0
     status_map = {}
     pattern = re.compile("[0-9][0-9][0-9]")
+    line_parsed2 = []
     try:
         for line in sys.stdin:
             """ jose """
-            line_parsed = line.split(" ")
+            if "-[" in line:
+                line = line.replace("-[", " - [")
+            line_parsed = line.split()
             if len(line_parsed) >= 8:
                 if pattern.match(line_parsed[7]):
                     key = line_parsed[7]
                     status_map[key] = status_map.get(key, 0) + 1
+                    if len(line_parsed) >= 9:
+                        size += int(line_parsed[8])
             counter += 1
-            if len(line_parsed) >= 9:
-                size += int(line_parsed[8])
+            
             if counter % 10 == 0:
                 print_result(size, status_map)
     except KeyboardInterrupt:
